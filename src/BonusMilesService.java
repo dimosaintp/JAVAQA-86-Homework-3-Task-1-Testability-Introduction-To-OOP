65a44c2199c67245d53ecf9455eca8a216800cb7
Dmitry <dimosaintp@gmail.com>
2024-05-01 01:01:00 +0300

    Ready

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,8 @@
 public class BonusMilesService {
     public int calculate(int ticket){
         int rub = 20; // для хранения количества рублей для одной бонусной мили.
-        int mile = (int) (ticket / rub);// Рассчитываем количество бонусных миль.
+        int mile;
+        mile = (int) (ticket / rub);// Рассчитываем количество бонусных миль.
         return mile;
     }
 }
